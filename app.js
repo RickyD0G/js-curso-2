b46012416c261131d95a0f51c16a7f35cf02696a
RickyD0G <112658818+RickyD0G@users.noreply.github.com>
2025-07-08 12:44:57 -0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+let numeroSecreto = gerarNumeroAleatorio();
+
+function exibirTextoNaTela(tag, texto)
+{
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do número segreto');
+exibirTextoNaTela('p', 'Escolha um número de 1 a 100');
+
+function verificarChute()
+{
+    console.log(numeroSecreto)
+}
+
+function gerarNumeroAleatorio()
+{
+    return parseInt(Math.random() * 100 + 1);
+}
