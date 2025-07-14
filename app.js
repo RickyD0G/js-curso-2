b533245bdd10b1ddf86edfd9ba46f744b2790ca7
RickyD0G <112658818+RickyD0G@users.noreply.github.com>
2025-07-14 13:30:03 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 0;
+
 
 function exibirTextoNaTela(tag, texto)
 {
@@ -11,10 +13,21 @@ exibirTextoNaTela('p', 'Escolha um número de 1 a 100');
 
 function verificarChute()
 {
-    console.log(numeroSecreto)
+    let chute = document.querySelector('input').value;
+    tentativas += 1;
+    if (chute == numeroSecreto)
+    {
+        exibirTextoNaTela('h1', 'Parabens você acertou na ' + tentativas + 'ª tentativa');
+        exibirTextoNaTela('p', 'Você acertou :)');
+    }else if(chute > numeroSecreto){
+        exibirTextoNaTela('h1', 'Poxa não foi dessa fez, o numero secreto é menor que '+ chute);
+    }else{
+        exibirTextoNaTela('h1', 'Poxa não foi dessa fez, o numero secreto é maior que '+ chute);
+    }
 }
 
 function gerarNumeroAleatorio()
 {
     return parseInt(Math.random() * 100 + 1);
 }
+
